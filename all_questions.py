2f7c8fe74db8cc2cce6b0eda0812209ec765cbd0
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-02-15 21:47:38 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -117,7 +117,7 @@ def question3():
     # float
     answer["(b) Gini, ID"] = 0.0
     answer["(c) Gini, Gender"] = 0.48
-    answer["(d) Gini, Car type"] = 0.1622
+    answer["(d) Gini, Car type"] = 0.1625
     answer["(e) Gini, Shirt type"] = 0.4914
 
     answer["(f) attr for splitting"] = "Car type"
